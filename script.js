4528ea609df8593d011950479c2586ab84287b23
halimafarsi <farsihalima@gmail.com>
2023-06-17 19:32:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,35 +2,81 @@
 const width = 800;
 const height = 600;
 
-// Crear el lienzo SVG
-const svg = d3.select("#map")
+// Crear el mapa con Leaflet
+const map = L.map("map").setView([46.5, 2.5], 5);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// Crear el lienzo SVG con D3.js
+const svg = d3.select(map.getPanes().overlayPane)
   .append("svg")
   .attr("width", width)
   .attr("height", height);
 
-// Proyección del mapa
-const projection = d3.geoMercator()
-  .center([2.5, 46.5])
-  .scale(1000)
-  .translate([width / 2, height / 2]);
+// Obtener la capa SVG del mapa de Leaflet
+const svgLayer = L.svgOverlay(svg.node(), map);
 
-// Ruta del mapa
-const path = d3.geoPath().projection(projection);
+// Capa de líneas
+const lineLayer = L.geoJSON().addTo(map);
 
-// Cargar los datos del mapa
-d3.json("datavisualiz-\docs\script.js").then(function(mapData) {
-  svg.selectAll("path")
-    .data(mapData.features)
-    .enter()
-    .append("path")
-    .attr("d", path)
-    .style("fill", "lightgray")
-    .style("stroke", "white");
+// Función para dibujar las líneas con D3.js
+function dibujarLineas(data) {
+  lineLayer.clearLayers();
+
+  data.forEach(function(d) {
+    const origen = [parseFloat(d.LongOrigen), parseFloat(d.LatOrigen)];
+    const destino = [parseFloat(d.LongDestino), parseFloat(d.LatDestino)];
+
+    const lineData = {
+      type: "LineString",
+      coordinates: [origen, destino]
+    };
+
+    const lineFeature = {
+      type: "Feature",
+      geometry: lineData,
+      properties: {
+        asunto: d.Asunto,
+        fecha: d.Fecha,
+        remitente: d.Remitente,
+        receptor: d.Receptor
+      }
+    };
+
+    lineLayer.addData(lineFeature);
+  });
+
+  lineLayer.setStyle({
+    color: "blue",
+    weight: 2
+  });
+
+  lineLayer.eachLayer(function(layer) {
+    layer.bindPopup(`
+      <h4>${layer.feature.properties.asunto}</h4>
+      <p>Remitente: ${layer.feature.properties.remitente}</p>
+      <p>Receptor: ${layer.feature.properties.receptor}</p>
+    `);
+  });
+}
+
+// Cargar los datos del CSV y dibujar las líneas
+d3.csv("test.csv").then(function(data) {
+  dibujarLineas(data);
 });
 
-// Obtener referencia al botón y al select
-const mostrarViajeBtn = document.getElementById("mostrarViajeBtn");
+// Función para filtrar los datos y actualizar las líneas
+function filtrarDatos(asunto) {
+  d3.csv("test.csv").then(function(data) {
+    const datosFiltrados = data.filter(d => asunto === "" || d.Asunto === asunto);
+    dibujarLineas(datosFiltrados);
+  });
+}
+
 const asuntoSelect = document.getElementById("asuntoSelect");
 
-// Agregar evento click al botón
-mostrarViajeBtn.addEventListener("click", mostrarViaje);
+asuntoSelect.addEventListener("change", function() {
+  const asuntoSeleccionado = asuntoSelect.value;
+  filtrarDatos(asuntoSeleccionado);
+});
